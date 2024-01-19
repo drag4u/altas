db2145476d9c302e2b7ff38ce9ca23e985831af9
drag4u <titas.siurskas@gmail.com>
2024-01-19 12:05:38 +0200

    Adding template xlsx support

--- core/api/controllers/generatorController.js
+++ core/api/controllers/generatorController.js
@@ -1,81 +1,112 @@
 const fs = require('fs');
+const path = require('path');
 const PizZip = require('pizzip');
 const Docxtemplater = require('docxtemplater');
+const XlsxTemplate = require('xlsx-template');
 
 module.exports = (logger, database, utils) => {
     return {
         GenerateCoC: async (req, res) => {
-			const {typeId} = req.params;
-			utils.ExecuteAction(res, `SELECT coc_file FROM type where type_id = ${typeId}`, rows => {
-				const cocFile = rows[0].coc_file;
-				
-				const content = fs.readFileSync('./data/files/' + cocFile, 'binary');
+            const { typeId } = req.params;
+            utils.ExecuteAction(res, `SELECT coc_file FROM type where type_id = ${typeId}`, async rows => {
+                const cocFile = rows[0].coc_file;
+                const filePath = path.join('./data/files', cocFile);
 
-				const zip = new PizZip(content);
+                // Determine file type
+                const isWordFile = cocFile.endsWith('.docx');
+                const isExcelFile = cocFile.endsWith('.xlsx');
 
-				const customParser = function(tag) {
-					return {
-						get(scope) {
-							if (scope[tag] === undefined) {
-								// If the tag is undefined, return a dash
-								return '-';
-							}
-							// Otherwise, return the actual value
-							return scope[tag];
-						}
-					};
-				};
-				
-				const options = {
-					parser: customParser,
-					paragraphLoop: true,
-					linebreaks: true,
-					delimiters: {
-						start: '%%',
-						end: '%%'
-					}
-				};
+                try {
+                    const content = fs.readFileSync(filePath, 'binary');
 
-				const doc = new Docxtemplater(zip, options);
-
-				let placeHolderData = {}
-				req.body.placeholderData.forEach(c => {
-					placeHolderData[`${c.placeholder}`] = c.data;
-				});
-				
-				doc.setData(placeHolderData);
-
-				try {
-					// Render the document (replace all occurrences of placeholders by their values)
-					doc.render()
-				}
-				catch (error) {
-					// Catch rendering errors
-					const e = {
-						message: error.message,
-						name: error.name,
-						stack: error.stack,
-						properties: error.properties,
-					}
-					
-					res.status(200).json({error: e});
-					return;
-				}
-				
-				const buf = doc.getZip().generate({type: 'nodebuffer'});
-				
-				const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-				let hash = '';
-				for (let i = 0; i < 5; i++) {
-					hash += characters.charAt(Math.floor(Math.random() * characters.length));
-				}
-
-				// Write the new .docx file
-				let filename = cocFile.slice(0, -11) + '-' + hash + '.docx';
-				fs.writeFileSync('./data/files/' + cocFile.slice(0, -11) + '-' + hash + '.docx', buf);
-				
-				res.status(200).json({fileName: filename});
-			});
+                    if (isWordFile) {
+                        await processWordFile(content, cocFile, req, res);
+                    } else if (isExcelFile) {
+                        await processExcelFile(content, cocFile, req, res);
+                    } else {
+                        res.status(400).send('Unsupported file type');
+                    }
+                } catch (error) {
+                    res.status(500).json({ error: error.message });
+                }
+            });
         }
     };
 };
+
+async function processWordFile(content, cocFile, req, res) {
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, getDocxOptions());
+    const placeHolderData = getPlaceholderData(req);
+
+    doc.setData(placeHolderData);
+
+    try {
+        doc.render();
+    } catch (error) {
+        res.status(200).json({ error: error });
+        return;
+    }
+
+    const buf = doc.getZip().generate({ type: 'nodebuffer' });
+    const filename = saveFile(buf, cocFile, 'docx');
+    res.status(200).json({ fileName: filename });
+}
+
+async function processExcelFile(content, cocFile, req, res) {
+    const template = new XlsxTemplate(content);
+    const values = getPlaceholderData(req);
+
+    // Attempt to substitute placeholders in each sheet until an error occurs
+    let sheetNumber = 1;
+    try {
+        while (true) {
+            template.substitute(sheetNumber, values);
+            sheetNumber++;
+        }
+    } catch (error) {
+        // Assuming error is thrown when no more sheets are available
+        console.log(`Processed ${sheetNumber - 1} sheets.`, error.message);
+    }
+
+    // Generate the final Excel file
+    const data = template.generate({ type: 'nodebuffer' });
+    const filename = saveFile(data, cocFile, 'xlsx');
+    res.status(200).json({ fileName: filename });
+}
+
+function getDocxOptions() {
+    return {
+        // Docxtemplater options
+        paragraphLoop: true,
+        linebreaks: true,
+        delimiters: {
+            start: '%%',
+            end: '%%'
+        }
+    };
+}
+
+function getPlaceholderData(req) {
+    let placeHolderData = {};
+    req.body.placeholderData.forEach(c => {
+        placeHolderData[c.placeholder] = c.data;
+    });
+    return placeHolderData;
+}
+
+function saveFile(data, originalFileName, extension) {
+    const hash = generateRandomString(5);
+    const filename = `${originalFileName.slice(0, -5)}-${hash}.${extension}`;
+    fs.writeFileSync(path.join('./data/files', filename), data);
+    return filename;
+}
+
+function generateRandomString(length) {
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
